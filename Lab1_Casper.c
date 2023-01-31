04141dcb88aad9878e1e3bf7e0b5b6229bd651ca
Casper <114100431+trollboss2572@users.noreply.github.com>
2023-01-31 17:00:44 -0500

    Update Lab1_Casper.c

--- Lab1_Casper.c
+++ Lab1_Casper.c
@@ -7,6 +7,7 @@
 #include <utmp.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <ctype.h>
 //TODO: Make sure to document that values are in Base 10
 
 //Prints the header, will be called regardless of the arguments afterwards. Returns the memory usage of the function in case it's needed in other print statements
@@ -15,10 +16,10 @@ int print_header(int sample_amt, int tick_time)
 	//Recieve the required data from libraries
 	struct rusage *pointer_curr_proc = malloc(sizeof(struct rusage));
 	getrusage(RUSAGE_SELF, pointer_curr_proc);
-	int mem_usage = (pointer_curr_proc->ru_maxrss) / 1000;
+	long mem_usage = (pointer_curr_proc->ru_maxrss);
 	//Print required statements
     printf("Number of Samples: %d -- every %d seconds\n", sample_amt, tick_time);
-    printf("Memory usage: %d\n", mem_usage);
+    printf("Memory usage: %d kilobytes\n", mem_usage);
     printf("_____________________________________________________\n");
 	free(pointer_curr_proc);
     return(mem_usage);
@@ -50,18 +51,34 @@ void print_system_samples()
 
 
 
-void print_user_section(int sample_amt)
+void print_user_section()
 {
-    struct utmp *pointer = malloc(sizeof(struct utmp));
-	char *username = pointer->ut_user;
-	char *dir;
-	char *operation;
+	setutent();
     printf("### Sessions/users ###\n");
-    for (int i = 0; i < sample_amt; i++)
+    for (struct utmp *pointer = malloc(sizeof(struct utmp)); getutent() != NULL; pointer = getutent())
     {
-        printf("%s          %s  (%s)", *username, *dir, *operation);
+		char *username = pointer->ut_user;
+		char *dir = pointer->ut_line;
+		char *operation = pointer->ut_host;
+        printf("%s          %s  (%s)", username, dir, operation);
     }
-
+	endutent();
+}
+//Helper function for print_system_ending, which calculates the difference in cpu_use given a string which comes from reading the /proc/stat file
+long get_cpu_use(char *cpu_info)
+{
+	long cpu_use = 0;
+	char *leftover;
+	//The first 3 numeric values of the first line of /proc/cpuinfo refer to the time taken by normal and nice processes from the user and time taken by processes executing in the kernel, which add up to all the time taken by processes total
+	cpu_use += strtol(cpu_info, &leftover, 10);
+	cpu_use += strtol(leftover, &leftover, 10);
+	cpu_use += strtol(leftover, &leftover, 10);
+	//Idle process line, which does not contribute to our total
+	strtol(leftover, &leftover, 10);
+	cpu_use += strtol(leftover, &leftover, 10);
+	cpu_use += strtol(leftover, &leftover, 10);
+	cpu_use += strtol(leftover, &leftover, 10);
+	return cpu_use;
 }
 
 //System ending requires us to print the cpu use and the number of CPUs total. Accepts a long that is the CPU use of the previous check iteration. In case old_cpu_use == 0, then there has been no previous check.
@@ -69,36 +86,31 @@ void print_user_section(int sample_amt)
 long print_system_ending(long old_cpu_use)
 {
 	//Initialize our variables to read from the file
-	char new_line[3];
+	char new_line[4];
 	int core_amt = 0;
-	char cpu_total[255];
-	char *leftover;
-	long cpu_use = 0;
+	char *cpu_total;
 	//Open the file for reading
 	FILE *file = fopen("/proc/cpuinfo", "r");
 	//Get the first line, which holds the cpu use data
 	fgets(cpu_total, 255, file);
-	//The first 3 numeric values of the first line of /proc/cpuinfo refer to the time taken by normal and nice processes from the user and time taken by processes executing in the kernel, which add up to all the time taken by processes total
-	cpu_use += strtol(cpu_total, &leftover, 10);
-	cpu_use += strtol(leftover, &leftover, 10);
-	cpu_use += strtol(leftover, &leftover, 10);
+	long cpu_use = get_cpu_use(cpu_total);
 	float diff_cpu_use = 0;
 	if (old_cpu_use != 0)
 	{
-		diff_cpu_use = (cpu_use - old_cpu_use) / 100;
+		diff_cpu_use = (cpu_use - old_cpu_use) / cpu_use;
 	}
-	//Get the first 3 characters for each line after the first. For each cpu, the new line will begin with the text "cpu" so we can list how many cpus there are that way
 	while(fgets(new_line, 3, file) != NULL)
 	{
+		//Get the first 3 characters for each line after the first. For each cpu, the new line will begin with the text "cpu" so we can list how many cpus there are that way
 		if (strcmp(new_line, "cpu") == 0)
 		{
 			core_amt++;
 		}
 	}
+	fclose(file);
     printf("Number of cores: %d\n", core_amt);
     printf("    Total cpu use: %.4f%%\n", diff_cpu_use);
     printf("____________________________\n");
-	fclose(file);
 	return(cpu_use);
 }
 
@@ -109,10 +121,10 @@ void print_system_info()
     uname(pointer);
     printf("### System Information ###\n");
     printf("System name: %s\n", pointer->sysname);
-    printf("Machine Name: %s\n", pointer->machine);
+    printf("Machine Name: %s\n", pointer->nodename);
     printf("Version: %s\n", pointer->version);
     printf("Release: %s\n", pointer->release);
-    printf("Architecture: %s\n", arch_name);
+    printf("Architecture: %s\n", pointer->machine);
     printf("___________________________\n");
 	free(pointer);
 }
@@ -123,15 +135,32 @@ void print_sequential(int samples, int tick_time, int ex_code)
 	int process_mem = print_header(samples, tick_time);
     //Code for printing everything
 	long cpu_use = 0;
+	print_header(samples, tick_time);
     if(ex_code == 0)
     {
-        print_system_use();
-        //TODO: Implement this part
+		for(int i = 0; i < samples; i++)
+        {
+            printf(">>>Iteration %d\n", i);
+            printf("Memory usage: %d\n", process_mem);
+            print_system_use();
+            for(int j = 0; j < i; j++)
+            {
+                printf("\n");
+            }
+            print_system_samples();
+            int rest_of_the_lines = samples - i;
+            for(int j = 0; j < rest_of_the_lines; j++)
+            {
+                printf("\n");
+            }
+			print_user_section();
+            cpu_use = print_system_ending(cpu_use);
+			sleep(tick_time);
+        }
     }
 	//Code for only printing system
 	if (ex_code == 1)
 	{
-		
         for(int i = 0; i < samples; i++)
         {
             printf(">>>Iteration %d\n", i);
@@ -150,28 +179,41 @@ void print_sequential(int samples, int tick_time, int ex_code)
             cpu_use = print_system_ending(cpu_use);
 			sleep(tick_time);
         }
-        print_system_info();
     }
     //Code for only printing users
     if (ex_code == 2)
     {
         for(int i = 0; i < samples; i++)
         {
+			printf(">>>Iteration %d\n", i);
             printf("Memory usage: %d\n", process_mem);
-            print_system_ending(cpu_use);
+			print_user_section();
+			cpu_use = print_system_ending(cpu_use);
+            sleep(tick_time);
         }
-        
-        sleep(tick_time);
     }
 	print_system_info();
 }
+
+void print_normal(int samples, int tick_time, int exclusion_code)
+{
+	char *pointer_old_info[samples];
+	for (int i = 0; i < samples; i++)
+	{
 		
+	}
+}
+
+
+
 
 int main(int argc, char **argv)
 {
 //Default values for samples and tick times (changes if user inputs some values)
 	int samples = 10;
+	bool samples_initialized = false;
 	int tick_time = 1;
+	bool tick_time_initialized = false;
 //Defaults for different metrics the user might want,initialized to false and changed to true only if the user explicitly says
 	bool system_only = false;
 	bool fancy = false;
@@ -188,21 +230,23 @@ int main(int argc, char **argv)
 		{
 			user_only = true;
 		}
-		if(strcmp(*(argv+i), "--graphics"))
+		if(strcmp(*(argv+i), "--graphics") == 0)
 		{
 			fancy = true;
 		}
-		if(strcmp(*(argv+i), "--sequential"))
+		if(strcmp(*(argv+i), "--sequential") == 0)
 		{
 			sequential = true;
 		}
-		if(strncmp(*(argv+i), "--samples=", sizeof(char) * 10))
+		if(strlen(*(argv+i)) == 1 && isdigit(**(argv+i)) && !samples_initialized);
 		{
-			samples = strtol(*(argv+i), &(argv + i), 10);
+			samples = strtol(*(argv+i), NULL, 10);
+			samples_initialized = true;
 		}
-		if(strncmp(*(argv+i), "--tdelay=", sizeof(char) * 9))
+		if(strlen(*(argv+i)) == 1 && isdigit(**(argv+i)) && samples_initialized && !tick_time_initialized)
 		{
 			tick_time = strtol(*(argv+i), NULL, 10);
+			tick_time_initialized = true;
 		}
 	}
     //Cover the case of user being an idiot and not reading documentation and inputting exclusive arguments
@@ -224,7 +268,6 @@ int main(int argc, char **argv)
 		{
 			exclusion_code = 2;
 		}
-		
 		if (sequential)
 		{
 			print_sequential(samples, tick_time, exclusion_code);
